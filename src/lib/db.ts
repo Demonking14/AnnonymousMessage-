73240da0ca14bdcccdb8f05e2c77ce1f57d5c05f
Demonking14 <adi.jha1424@gmail.com>
2025-09-12 11:00:24 +0530

    Implement database connection logic using mongoose

---
+++ src/lib/db.ts
@@ -0,0 +1,31 @@
+import mongoose, { Types }  from "mongoose";
+
+type connectionObject =  {
+    isConnected ? : number
+
+}
+
+const connection : connectionObject = {};
+
+async function dbConnect ():Promise<void>{
+   if(connection.isConnected){
+    console.log("Database is alreaady connected ")
+   }
+
+   try {
+    const connecting = await mongoose.connect(process.env.MONGO_URI || '');
+    console.log(connecting);
+    connection.isConnected =connecting.connections[0].readyState;
+
+    console.log("Db connected successfully");
+    
+   } catch (error) {
+    console.log("Error in connecting to mongoDB");
+
+    process.exit(1);
+    
+   }
+
+}
+
+export default dbConnect;
