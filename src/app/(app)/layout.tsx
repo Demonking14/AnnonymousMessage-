01412659f58e72a456dc6c045a80ad70bf2d415f
Demonking14 <adi.jha1424@gmail.com>
2025-12-03 17:10:44 +0530

    feat: add AuthSessionProvider and Navbar components to RootLayout for authentication context

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,7 +1,10 @@
+import "../globals.css";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 import { Toaster } from "@/components/ui/sonner"
+import Navbar from "@/components/Navbar";
+import { AuthSessionProvider } from "@/components/AuthSessionProvider";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +31,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+      
+        <AuthSessionProvider>
+            <Navbar/>
+           {children}
+        </AuthSessionProvider>
+       
           <Toaster />
       </body>
     </html>
